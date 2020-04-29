b7e720fee0486ddef95c35ff6d13d742d20b2c7d
Galigne <joel.galigne@gmail.com>
2020-04-29 17:18:58 +0200

    Add locate a bird page

---
+++ app/src/main/java/com/example/birdstagram/LocateBirdActivity.java
@@ -0,0 +1,14 @@
+package com.example.birdstagram;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LocateBirdActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.locate_a_bird);
+    }
+}
