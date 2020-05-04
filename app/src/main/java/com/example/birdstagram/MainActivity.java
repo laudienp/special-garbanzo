ae1738782c4d22402511172e2a0941c5f5219d43
Asaréel <asareel56@gmail.com>
2020-05-04 20:44:23 +0200

    #2 Update
    Signed-off-by: Asaréel <asareel56@gmail.com>

--- app/src/main/java/com/example/birdstagram/MainActivity.java
+++ app/src/main/java/com/example/birdstagram/MainActivity.java
@@ -13,6 +13,9 @@ import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.birdstagram.inscription.ProfileActivity;
+import com.example.birdstagram.inscription.SignUpActivity;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -35,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
         };
 
         if (hasPermissions(this, PERMISSIONS)){
-            Intent intent = new Intent(getApplicationContext(), startingActivity);
+            Intent intent = new Intent(getApplicationContext(), SignUpActivity.class);
             startActivity(intent);
         } else {
             requestPermissions(PERMISSIONS, 1);
