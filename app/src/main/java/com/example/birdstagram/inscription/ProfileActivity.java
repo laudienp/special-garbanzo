c169fca1dc63596981cc210f3b69023b10ecd2a5
Asaréel <asareel56@gmail.com>
2020-05-06 17:34:34 +0200

    #2 Added profile picture upload
    Signed-off-by: Asaréel <asareel56@gmail.com>

--- app/src/main/java/com/example/birdstagram/inscription/ProfileActivity.java
+++ app/src/main/java/com/example/birdstagram/inscription/ProfileActivity.java
@@ -3,39 +3,90 @@ package com.example.birdstagram.inscription;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Debug;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import com.example.birdstagram.R;
 
-public class ProfileActivity extends AppCompatActivity {
+import java.io.File;
 
-    private static final int RESULT_LOAD_IMAGE = 0;
+public class ProfileActivity extends AppCompatActivity {
+    private static final int PICK_IMAGE = 2;
+    private static final int CAMERA_REQUEST = 1888;
+    private static final int MY_CAMERA_PERMISSION_CODE = 100;
+
+
+    private ImageView imageProfile;
+    private EditText name;
+    private EditText age;
+    private EditText email;
+    private EditText password;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        imageProfile = findViewById(R.id.profil_image);
+
+        name = findViewById(R.id.name_editView);
+        age = findViewById(R.id.age_editView);
+        email = findViewById(R.id.email_editView);
+        password = findViewById(R.id.password_editView);
+
+        imageProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
+                {
+                    requestPermissions(new String[]{Manifest.permission.CAMERA}, MY_CAMERA_PERMISSION_CODE);
+                }
+                else
+                {
+                    Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE); // directly open camera
+                    startActivityForResult(cameraIntent, CAMERA_REQUEST);
+                    /*Intent intent = new Intent(Intent.ACTION_GET_CONTENT); //Directly choose from gallery
+                    Intent intent2 = new Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI); //
+                    intent.setType("image/*");
+                    startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE);*/
+                }
+            }
+        });
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
         super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == RESULT_OK && null != data) {
+        if (requestCode == 2 && resultCode == RESULT_OK && null != data) {
             Uri selectedImage = data.getData();
-            String[] filePathColumn = { MediaStore.Images.Media.DATA };
-            Cursor cursor = getContentResolver().query(selectedImage,filePathColumn, null, null, null);
-            cursor.moveToFirst();
-            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
-            String picturePath = cursor.getString(columnIndex);
-            cursor.close();
-            ImageView imageView = findViewById(R.id.profil_image);
-            imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+
+            imageProfile.setBackgroundResource(0);
+            imageProfile.setImageURI(selectedImage);
+        }
+        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK)
+        {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            imageProfile.setBackgroundResource(0);
+            imageProfile.setImageBitmap(photo);
         }
     }
 }
