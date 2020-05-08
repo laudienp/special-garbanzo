dde6d70253a45aeed16042e8b3b87afe5672ea55
laudienp <paul.laudien@etu.unice.fr>
2020-05-08 17:48:27 +0200

    add frag and can share an image with share button

--- app/src/main/java/com/example/birdstagram/MainActivity.java
+++ app/src/main/java/com/example/birdstagram/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(getApplicationContext(), Map.class);
+        Intent intent = new Intent(getApplicationContext(), GalleryActivity.class);
         startActivity(intent);
     }
 
