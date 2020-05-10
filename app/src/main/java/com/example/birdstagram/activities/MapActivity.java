58445ddb88765275cd1cd32f573ec52f20210c34
laudienp <paul.laudien@etu.unice.fr>
2020-05-10 16:54:30 +0200

    fix map

--- app/src/main/java/com/example/birdstagram/activities/MapActivity.java
+++ app/src/main/java/com/example/birdstagram/activities/MapActivity.java
@@ -1,12 +1,18 @@
 package com.example.birdstagram.activities;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.view.Window;
 import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.example.birdstagram.R;
 
@@ -14,9 +20,13 @@ import org.osmdroid.api.IMapController;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
+import org.osmdroid.config.Configuration;
+
+import java.util.ArrayList;
 
 public class MapActivity extends AppCompatActivity {
 
+    private final int REQUEST_PERMISSIONS_REQUEST_CODE = 1;
     private MapView map;
     private ImageButton menuButton;
     private ImageButton cameraButton;
@@ -44,9 +54,21 @@ public class MapActivity extends AppCompatActivity {
             }
         });
 
+        Context ctx = getApplicationContext();
+        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
+
+
         map = findViewById(R.id.map);
         map.setTileSource(TileSourceFactory.MAPNIK);
-        map.setBuiltInZoomControls(true);
+        //map.setBuiltInZoomControls(true);
+
+        requestPermissionsIfNecessary(new String[]
+                {
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE
+                });
+
+        map.setMultiTouchControls(true);
+
         GeoPoint startPoint = new GeoPoint(43.130190, 5.923105);
         IMapController mapController = map.getController();
         mapController.setZoom(16.0);
@@ -63,5 +85,24 @@ public class MapActivity extends AppCompatActivity {
     public void onResume() {
         super.onResume();
         map.onResume();
+    }
+
+    private void requestPermissionsIfNecessary(String[] permissions)
+    {
+        ArrayList<String> permissionsToRequest = new ArrayList<>();
+        for(String permission : permissions)
+        {
+            if(ContextCompat.checkSelfPermission(this, permission)
+                != PackageManager.PERMISSION_GRANTED)
+                permissionsToRequest.add(permission);
+        }
+        if(permissionsToRequest.size() > 0)
+        {
+            ActivityCompat.requestPermissions(
+                    this,
+                    permissionsToRequest.toArray(new String[0]),
+                    REQUEST_PERMISSIONS_REQUEST_CODE
+            );
+        }
     }
 }
