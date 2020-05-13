fa392dd468d25e485b6c872175f18692d6f6b936
laudienp <paul.laudien@etu.unice.fr>
2020-05-13 12:39:56 +0200

    add more bird img

--- app/src/main/java/com/example/birdstagram/fragments/PostAdapter.java
+++ app/src/main/java/com/example/birdstagram/fragments/PostAdapter.java
@@ -16,13 +16,23 @@ public class PostAdapter extends ArrayAdapter<Post>
     @Override
     public View getView(int position, View convertView, ViewGroup parent)
     {
+        int[] images = new int[]{
+          R.drawable.colibri,
+          R.drawable.corbeau,
+          R.drawable.gros_oiseau,
+          R.drawable.oiseau_colore
+        };
         LayoutInflater inflater = (LayoutInflater)getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
         View postView = inflater.inflate(R.layout.gallery_list_element, parent, false);
         ImageView imageView = (ImageView)postView.findViewById(R.id.glistImage);
 
         if(convertView == null)
-            imageView.setImageResource(R.drawable.colibri);
+        {
+            int selectedImage = position%4;
+            imageView.setImageResource(images[selectedImage]);
+        }
+
         else
             postView = (View)convertView;
 
--- app/src/main/java/com/example/birdstagram/fragments/PostFragment.java
+++ app/src/main/java/com/example/birdstagram/fragments/PostFragment.java
@@ -46,7 +46,7 @@ public class PostFragment extends Fragment
             public void onClick(View v)
             {
                 Intent sharingIntent = new Intent(Intent.ACTION_SEND);
-                Uri screenshotUri = Uri.parse("android.resource://com.example.birdstagram/drawable/" + R.drawable.colibri);
+                Uri screenshotUri = Uri.parse("android.resource://com.example.birdstagram/drawable/" + R.drawable.colibri + ".jpg");
 
                 sharingIntent.setType("image/*");
                 sharingIntent.putExtra(Intent.EXTRA_STREAM, screenshotUri);
